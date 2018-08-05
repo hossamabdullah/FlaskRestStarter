7b93c78b5772bfc6faad80b779bc58e7eae09d14
Hossameldeen <hossamabdalh@gmail.com>
2018-08-05 17:03:53 +0200

    use flask_restplus with builtin integration with swagger

--- app/views.py
+++ app/views.py
@@ -1,24 +1,34 @@
 import json
 from app import app
 from flask import request, jsonify
-from flask_restful import Api, Resource, reqparse
 from werkzeug.datastructures import FileStorage
 import os
 from app.Sentiment import tweetSearch, tweetSentimentAnalysis
 
-api = Api(app)
+from flask_restplus import Api, Resource, fields, reqparse
+
+api = Api(app, version='1.0', title='Sentiment API',
+    description='API for performing sentiment analysis',
+)
 
 from app.Utils import Util
 
+@api.route('/sentiment', endpoint = 'SentimentServiceAPI')
 class SentimentServiceAPI(Resource):
-    def __init__(self):
+
+    @api.doc(params={'keyword':'keyword to perform sentiment analysis on'})
+    def post(self):
         self.parser = reqparse.RequestParser()
         self.parser.add_argument('keyword', required=True, help="keyword is required")
-
-    def post(self):
         args = self.parser.parse_args()
         tweetSearch(args["keyword"])
         sentiment = tweetSentimentAnalysis()
         return sentiment, 200
 
-api.add_resource(SentimentServiceAPI, '/sentiment', endpoint = 'SentimentServiceAPI')
+
+
+@api.route('/test', endpoint="test")
+class ping(Resource):
+
+    def get(self):
+        return "hello", 200
