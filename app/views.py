1c3e9d9a40310b4da5ac07bf8ce6fdb7ad3d0139
hossameldeen <hossamabdalh@gmail.com>
2018-08-08 18:15:55 +0200

    add history of sentinces endpoint

--- app/views.py
+++ app/views.py
@@ -30,7 +30,7 @@ class ValuesServicesAPI(Resource):
         print("***************************")        
         return output, 200
 
-@api.route('/history', endpoint = 'SentimentApi')
+@api.route('/historyOfTopics', endpoint = 'SentimentApi')
 class BlockChainTopic(Resource):
 
     @api.doc(params={'topic':'keyword to perform sentiment analysis on'})
@@ -43,3 +43,16 @@ class BlockChainTopic(Resource):
 
         return result, 200
 
+@api.route('/historyOfSentences', endpoint = 'historyOfSentences')
+class BlockChainSentence(Resource):
+
+    @api.doc(params={'topic':'topic to perform sentiment analysis on'})
+    def get(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('topic', required=True, help="topic is required")
+        args = self.parser.parse_args()
+        blockchain=Blockchain()
+        print(args["topic"])
+        result=blockchain.return_sentence(args["topic"])
+
+        return result, 200
